dbdf99c43f520c8b693fc65a36ed230f96e42c06
Firoz Shaikh <gitfiroz695@gmail.com>
2023-08-16 12:36:10 +0530

    Android MVVM Architecture Complete Example

---
+++ app/src/main/java/com/firozpocyt/androidmvvmarchitecturecompleteexample/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.firozpocyt.androidmvvmarchitecturecompleteexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.firozpocyt.androidmvvmarchitecturecompleteexample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var mainViewModel: MainViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        val dao = QuoteDatabase.getDatabase(applicationContext).quoteDao()
+        val repository = QuoteRepository(dao)
+        mainViewModel = ViewModelProvider(this, MainViewModelFactory(repository))[MainViewModel::class.java]
+        mainViewModel.getQuotes().observe(this, Observer {
+            binding.quotes = it.toString()
+        })
+        binding.btnAddQuote.setOnClickListener {
+            val quote = Quote(0, "This is testing", "Testing")
+            mainViewModel.insertQuote(quote)
+        }
+    }
+}
---
+++ app/src/main/java/com/firozpocyt/androidmvvmarchitecturecompleteexample/QuoteRepository.kt
@@ -0,0 +1,14 @@
+package com.firozpocyt.androidmvvmarchitecturecompleteexample
+
+import androidx.lifecycle.LiveData
+
+class QuoteRepository(private val quoteDao: QuoteDao) {
+
+    fun getQuotes(): LiveData<List<Quote>>{
+        return quoteDao.getQuotes()
+    }
+
+    suspend fun insertQuote(quote: Quote){
+        quoteDao.insertQuote(quote)
+    }
+}
